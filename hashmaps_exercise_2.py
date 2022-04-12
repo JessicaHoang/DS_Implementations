d8c4b2f4c35c8a0a1e67e16e6ad35f6aabb6728f
Jessica Hoang <jessica.hoang@neatorobotics.com>
2022-04-12 11:43:38 -0700

    finsihed up exercise#2

--- hashmaps_exercise_2.py
+++ hashmaps_exercise_2.py
@@ -1,10 +1,12 @@
 from hashmaps import *
+import time
 import csv
 
-import pandas as pd
 t = HashTable()
+start_time = time.time()
 
 # my implementation
+# utilizes hashtable
 with open('nyc_weather.csv', newline='') as csvfile:
     reader = csv.DictReader(csvfile)
     for idx, t in enumerate(reader):
@@ -12,8 +14,24 @@ with open('nyc_weather.csv', newline='') as csvfile:
             print(t['temperature(F)'])
         if t['date'] == 'Jan 4':
             print(t.__getitem__('temperature(F)'))
+print("--- %s seconds ---" % (time.time() - start_time)) # mine is faster
 
-    
+# their implementation
+# utilizes dictionary
+weather_dict = {}
+with open("nyc_weather.csv", 'r') as f:
+    for line in f:
+        tokens = line.split(',')
+        day = tokens[0]
+        try:
+            temperature = int(tokens[1])
+            weather_dict[day] = temperature
+        except:
+            print("Invalid temperature. ignore the row")
+
+print(weather_dict['Jan 4'])
+print(weather_dict['Jan 9'])
+print("--- %s seconds ---" % (time.time() - start_time))
 
 
     
